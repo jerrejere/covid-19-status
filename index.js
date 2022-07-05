079fe32cb8d7c6d46e116139209d7e7eb063d082
jerryjohns <jerry.johns@student.moringaschool.com>
2022-07-05 12:09:02 +0300

    add

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+var myForm = document.getElementById('myForm')
+
+myForm.addEventListener('submit',function(e){ 
+
+    e.preventDefault()
+
+    var country = document.getElementById('country').value
+alert(country) 
+
+
+
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '4f05868062msh8d5b131431edda5p14e716jsn4e745ec58bc5',
-		'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+		'X-RapidAPI-Key': '957b1df485mshcc07ded3acb036cp1f5fa0jsnc27cef59dfa0',
+		'X-RapidAPI-Host': 'covid-19-coronavirus-statistics.p.rapidapi.com'
 	}
 };
 
-fetch('https://covid-193.p.rapidapi.com/countries', options)
+fetch(`https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=${country}`, options)
 	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.then(data => { 
+		console.log(data.data.covid19Stats[0]);
+		
+
+        document.getElementById('death').innerHTML = data.data.covid19Stats[0].deaths ?? 0
+        document.getElementById('confirmed').innerHTML = data.data.covid19Stats[0].confirmed || 0 
+        document.getElementById('recovered').innerHTML = data.data.covid19Stats[0].recovered ?? 0
+
+		
+    })
+	
+})
